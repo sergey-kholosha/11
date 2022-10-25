4efefdcaf736e6688cf2a005f46fe18795158646
Yevhen Herasymenko <herasymenko.yevhen@gmail.com>
2022-10-25 16:20:16 +0300

    lab 1

---
+++ LabaKholosha/LabaKholosha/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  LabaKholosha
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+    }
+}
---
+++ LabaKholosha/LabaKholosha/UI/Lab1/PicturesViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PicturesViewController.swift
+//  LabaKholosha
+//
+//  Created by Yevhen Herasymenko on 25.10.2022.
+//
+
+import UIKit
+
+class PicturesViewController: UIViewController {
+
+    private enum Constants {
+        static let minIndex = 0
+        static let maxIndex = 5
+    }
+
+    private struct State {
+        var index: Int = 0
+    }
+
+    private var state = State() {
+        didSet {
+            updateUI()
+        }
+    }
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    private func updateUI() {
+        nextButton.isEnabled = state.index < Constants.maxIndex
+        previousButton.isEnabled = state.index > Constants.minIndex
+        imageView.image = UIImage(named: "pictures/\(state.index)")
+    }
+
+}
+
+// MARK: - Actions
+extension PicturesViewController {
+
+    @IBAction func didTapNext() {
+        state.index = calculatedIndex(from: state.index + 1)
+    }
+
+    @IBAction func didTapPrevious() {
+        state.index = calculatedIndex(from: state.index - 1)
+    }
+
+    private func calculatedIndex(from index: Int) -> Int {
+        return (min(max(Constants.minIndex, index), Constants.maxIndex))
+    }
+
+}
